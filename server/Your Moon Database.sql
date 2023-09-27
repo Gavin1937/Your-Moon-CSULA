74d136be205c0afaeea0614be669e5f06a4ef7a3
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-09-27 16:39:57 -0700

    update db schema

--- server/Your Moon Database.sql
+++ server/Your Moon Database.sql
@@ -1,15 +1,30 @@
-select * from LunarImageDB;
 
--- truncate LunarImageDB; 
+CREATE TABLE IF NOT EXISTS LunarImageDB(
+    id                  INT              AUTO_INCREMENT PRIMARY KEY,
+    timestamp           INT              NOT NULL, -- unix timestamp
 
--- create table LunarImageDB(
--- 	id int auto_increment PRIMARY KEY,
---     img_name varchar(50),
---     img_type varchar(10),
---     img_file varchar(255),
---     altitude int,
---     longitude int not null,
---     latitude int not null,
---     m_time time,	# HH:MM:SS
---     m_date date		# YYYY-MM-DD
--- );
+    -- user data
+    user_token          VARCHAR(256)     NOT NULL, -- OAuth 2.0 token/credential
+    user_email          VARCHAR(256)     NOT NULL,
+
+    -- image metadata
+    img_name            VARCHAR(50)      NOT NULL,
+    img_type            VARCHAR(50)      NOT NULL, -- MIME type
+    img_uri             VARCHAR(256)     NOT NULL,
+    img_altitude        FLOAT            NOT NULL,
+    img_longitude       FLOAT            NOT NULL,
+    img_latitude        FLOAT            NOT NULL,
+
+    -- moon metadata
+    moon_detect_flag    INT              NULL, -- reserved flag
+    moon_exist_flag     BOOL             NULL, -- flag, img contains moon
+    moon_loc_x          INT              NULL, -- circle center x
+    moon_loc_y          INT              NULL, -- circle center y
+    moon_loc_r          INT              NULL, -- circle radius
+
+    -- instrument metadata
+    instrument_type     VARCHAR(25)     NOT NULL, -- "phone", "camera", "phone+telescope", "camera+telescope"
+    instrument_make     VARCHAR(256)    NULL,
+    instrument_model    VARCHAR(256)    NULL,
+
+);

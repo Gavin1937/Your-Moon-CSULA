c9316fd8bebc5d11da06ab37b8d4e92b100a5202
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-10-10 10:14:41 -0700

    fix sql error

--- server/Your Moon Database.sql
+++ server/Your Moon Database.sql
@@ -1,25 +1,28 @@
 
 CREATE DATABASE IF NOT EXISTS YourMoonDB;
 
+USE YourMoonDB;
+
 CREATE TABLE IF NOT EXISTS Users(
     user_id             INT              AUTO_INCREMENT PRIMARY KEY NOT NULL,
     user_email          VARCHAR(256)     NOT NULL, -- encrypted email
     user_upload_count   INT              DEFAULT 0,
-    user_flag_count     INT              DEFAULT 0,
+    user_flag_count     INT              DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS Instruments(
     inst_id             INT              AUTO_INCREMENT PRIMARY KEY NOT NULL,
     inst_type           VARCHAR(25)      NOT NULL, -- "phone", "camera", "phone+telescope", "camera+telescope"
     inst_make           VARCHAR(256)     NULL,
-    inst_model          VARCHAR(256)     NULL,
+    inst_model          VARCHAR(256)     NULL
     -- other instrument metadata...
 );
 
 CREATE TABLE IF NOT EXISTS Images(
     img_id              INT              AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    inst_id             INT              FOREIGN KEY NOT NULL,
+    inst_id             INT              NOT NULL,
     metadata_id         INT              DEFAULT -1, -- reserved id
+    FOREIGN KEY (inst_id) REFERENCES Instruments(inst_id),
     
     -- image metadata
     img_name            VARCHAR(50)      NOT NULL,
@@ -35,5 +38,5 @@ CREATE TABLE IF NOT EXISTS Images(
     moon_exist_flag     BOOL             NULL,     -- flag, img contains moon
     moon_loc_x          INT              NULL,     -- circle center x
     moon_loc_y          INT              NULL,     -- circle center y
-    moon_loc_r          INT              NULL,     -- circle radius
+    moon_loc_r          INT              NULL      -- circle radius
 );

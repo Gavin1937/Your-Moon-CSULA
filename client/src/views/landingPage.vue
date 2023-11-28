ec11c5c89017a0e15d33e865e176a8b626c4f5d8
Olga <oherna59@calstatela.edu>
2023-11-27 17:28:36 -0800

    Frontend Changes
    Updated landingPage styling

--- client/src/views/landingPage.vue
+++ client/src/views/landingPage.vue
@@ -1,135 +1,128 @@
 <script setup>
 import config from "../../config/config.json";
+import { ref } from 'vue';
 
+const isExpanded = ref(false);
 
-async function google() {
-    window.open(config.backend_url + "/api/auth/google", "_self");
-}
+// async function google() {
+//   window.open(config.backend_url + "/api/auth/google", "_self");
+// }
 
-async function github() {
-    window.open(config.backend_url + "/api/auth/github", "_self");
+// OAUTH 2.0 not implemented yet
+// async function github() {
+//   window.open(config.backend_url + "/api/auth/github", "_self");
+// }
+
+function toggleExpand() {
+  isExpanded.value = !isExpanded.value;
 }
 </script>
 
 <template>
-  <main class="main">
-    <div class="columns">
-      <div class="column is-two-thirds-desktop is-6-tablet has-text-white custom-box">
-        <h1 class="mt-4">Welcome</h1>
-        <p>
-          Welcome to YourMoon, a web application inviting users to submit images of their Moon. Explore your lunar captures with labels from NASA JPL's MoonTrek, aiding the 'MoonTrek AR' team in gathering data.
-        </p>
-        <h1>What is YourMoon?</h1>
-        <p>
-          YourMoon is a vital component of an ongoing Capstone Senior Design project, a collaborative effort between NASA's JPL and California State University, Los Angeles (CSULA). The primary objective is to create a user-friendly interface for <a href='https://yourlinktomoontrek.com'>Moon Trek</a>, a powerful NASA tool empowering users to delve into the mysteries of the lunar by offering user-friendly tools that simplify browsing, data layering, and detailed feature searches. YourMoon invites users to contribute their Moon images and utilize NASA JPL's MoonTrek data, aiding the 'MoonTrek AR' team in their data collection endeavors. The metadata provided by users is kept anonymous and used strictly for research purposes.
-          <img src="../assets/moon_overlay.png" alt="moon" class="moon_img">
-        </p> 
-      </div>
+<body class="bodywrap">
+  <div class="page-wrap">
+    <div class="the-content">
+      <div class="row">
+        <!-- LeftHand Column -->
+        <div class="col-xs-12 col-md-8">
+          <h1>Welcome</h1>
+          <p>Welcome to YourMoon, a web application inviting users to submit images of their Moon. 
+             Explore your lunar captures with labels from NASA JPL's MoonTrek, aiding the 'MoonTrek 
+             AR' team in gathering data.</p>
 
-      <div class="column is-one-third-desktop is-6-tablet has-text-white custom-box">
-        <article class="message">
-          <div class="message-header">
-            <p class="message-title has-text-white">UPLOAD YOUR MOON IMAGES</p>
+          <h1>What is YourMoon?</h1>
+          <p>YourMoon is a vital component of an ongoing Capstone Senior Design project, a 
+          collaborative effort between NASA's JPL and California State University, Los Angeles (CSULA). The primary objective is to create a user-friendly interface for <a href='https://trek.nasa.gov/moon/'>Moon Trek</a>, a powerful NASA tool empowering users to delve into the mysteries of the lunar by offering user-friendly tools that simplify browsing, data layering, and detailed feature searches. YourMoon invites users to contribute their Moon images and utilize NASA JPL's MoonTrek data, aiding the 'MoonTrek AR' team in their data collection endeavors. The metadata provided by users is kept anonymous and used strictly for research purposes.</p>
+          <p><img src="../assets/moon_overlay.png" alt="moon" class="moon_img"></p>
+        </div>
+
+        <!-- RightHand Column -->
+        <div class="col-xs-6 col-md-4">
+          <!-- Upload Card -->
+          <div class="card">
+            <div class="card-header text-left">
+              UPLOAD YOUR MOON
+            </div> 
+            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+              <p class="card-text">For enhanced security measures, we kindly ask you to log in using your preferred email address. This ensures a secure interaction for both you and our system. Thank you.</p>
+              <button type="button" class="btn btn-secondary mt-1 btn-md btn-block btn-warning" @click="google">Sign in with Email</button>
+            </div>
           </div>
-          <div class="message-body has-text-white">
-            <p>
-              For enhanced security measures, we kindly ask you to log in using your 
-              preferred email address. This ensures a secure interaction for both you 
-              and our system. Thank you.
-              <br /><br />
-              <div style="text-align: center;">
-                <button class="button is-warning is-rounded" @click="google">Sign in</button>
+
+          <!-- Additional Card -->
+          <div class="card mt-4">
+            <div class="card-header text-left">
+              MOONTREK AR TEAM
+            </div> 
+            <div class="card-body d-flex justify-content-center align-items-center flex-column">
+               <p class="card-text">For enhanced security measures, we kindly ask you to log in using your preferred email address. This ensures a secure interaction for both you and our system. Thank you.</p>
+              
+              <div v-if="isExpanded">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+                <p>Lorem ipsum dorlor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
               </div>
-            </p>
+              <button type="button" class="btn btn-secondary mt-1btn-md btn-block btn-warning" @click="toggleExpand">{{ isExpanded ? 'Read Less' : 'Read More' }}</button>
+            </div>
           </div>
-        </article>
-        <article class="message">
-          <div class="message-header">
-            <p class="message-title has-text-white">MoonTrek AR</p>
-          </div>
-          <div class="message-body has-text-white">
-            <p>
-              MoonTrek AR, an innovative project, aims to advance lunar education and exploration using augmented reality (AR). 
-              It generates precise 3D models, allowing users to immerse themselves in the Moon's surface at the time of their photos.
-              By integrating NASA's Moon Trek data, it offers overlays from missions and telescopes. Explore past and current lunar missions, 
-              historic landmarks, and craters in an interactive portal. To learn more, visit
-              <a href="https://ascent.cysun.org/project/project/view/208">
-                <button class="button is-warning is-rounded">MoonTrek AR</button>
-              </a>
-            </p>
-          </div>
-        </article>
+        </div>
       </div>
     </div>
-  </main>
+  </div>
+</body>
 </template>
 
-<style>
-.main {
-  padding: 20px;
-  padding-left: 75px;
-  padding-right: 75px;
-  background-image: url("../assets/galaxy_01.png");
+<style scoped>
+body, html {
+  background: #212529;
+  margin: 0; 
 }
 
-.custom-box {
-  padding-top: 1px;
-  padding-bottom: 10px;
-  padding-left: 20px;
-  margin-bottom: 20px;
-  background-color: #050608;
-  border: 2px solid #3a3a3a;
-  border-radius: 5px;
+.the-content {
+  float: left;
+  width: 100%;
+}
+
+.row {
+  padding-top: 25px;
+  padding-left: 55px;
+  padding-right: 30px;
+  color: #ffff;
+  background: #212529;
+}
+
+.upload h4 {
+  color: #ffff;
+  font-size: 1.5rem;
+}
+
+.card {
+  border: none;
+  background-color: #303639;
+  font-weight: bold;
+  text-align: left;
+  margin: 0px -16px;
+  padding-bottom: 20px;
+  width: calc(100% + 32px);
+ 
+}
+
+p {
+  margin: 0 0 1.5em 0;
+  margin-bottom: 1.5em;
+  padding-right: 25px;
+}
+
+.wrap {
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  padding: 0 16px;
 }
 
 .moon_img {
-  max-width: 100%;
-  height: auto;
   display: block;
-  margin-top: 1.5rem;
-  margin-right: auto;
-}
-
-.button {
-  margin-left: auto;
-  margin-right: auto;
-  background-color: #645394;
-  color: #ffffff;
-}
-
-.columns {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.column {
-  flex: 1 0 100%;
-}
-
-@media (min-width: 769px) {
-  .column {
-    flex: 1 0 50%;
-  }
-}
-
-.message {
-  padding-top: 20px;
-  padding-left: 10px;
-  padding-right: 10px;
-  background-color: rgba(2, 4, 3, 0.7);
-  margin-bottom: 2px;
-  color: #ffffff;
-}
-
-.message-header {
-  background-color: #232022;
-  font-size: 1.25rem;
-  font-weight: bold;
-  font-family: 'Roboto', monospace;
-
-}
-
-.message-body {
-  background-color: #101026;
+  margin: 0 auto;
 }
 </style>

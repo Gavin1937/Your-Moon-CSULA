53eeced0b46b37d221b724f06bd024fab85774c9
Joan Zaldivar <nzaldivar1187@gmail.com>
2024-02-05 12:01:10 -0800

    simple contact page update

--- client/src/views/contactPage.vue
+++ client/src/views/contactPage.vue
@@ -1,20 +1,117 @@
 <template>
-<body>
+  <body>
+    <div class="container">
+      <div class="screen">
+        <div class="card mt-4">
+          <div class="card-header text-center">Contact us</div>
+          <div
+            class="card-body d-flex justify-content-center align-items-center flex-column"
+          ></div>
 
-<div>
-    <p> dont contact us pls</p>
-</div>
-
-
-</body>
+          <div class="contact-form">
+            <div class="contact-form-group">
+              <input class="contact-form-edit" placeholder="name" />
+            </div>
+            <div class="contact-form-group">
+              <input class="contact-form-edit email" placeholder="Email" />
+            </div>
+            <div class="contact-form-group message">
+              <textarea
+                class="contact-form-edit-message"
+                placeholder="Message"
+                rows="5"
+                cols="30"
+              ></textarea>
+            </div>
+            <div class="contact-form-group buttons">
+              <button class="contact-form-button">Cancel</button>
+              <button class="contact-form-button">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
+
 <style scoped>
-body, html {
-    background: #212529;
-    margin: 0; 
-  }
-p {
-    color: white;
+body,
+button,
+input {
+  background: linear-gradient(to right, #212529 0%, 212526 100%);
+
+  font-size: 1.3em;
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: 1.4px;
 }
 
+.container {
+  flex: 3 3 1000px;
+  margin: auto;
+  padding: 2px;
+}
+
+p {
+  color: white;
+}
+
+.card {
+  border: none;
+  background-color: #303639;
+  font-weight: bold;
+  text-align: left;
+  margin: auto;
+  padding-bottom: 20px;
+  width: calc(80%);
+}
+
+.contact-form-group {
+  margin-bottom: 15px;
+  text-align: center;
+}
+
+.contact-form-edit.email{
+    text-transform: inherit;
+}
+.contact-form-group.message {
+  margin-top: 40px;
+}
+.contact-form-group.buttons {
+  margin-bottom: 0;
+  text-align:center;
+}
+
+.contact-form-edit-message {
+  width: 30%;
+  padding: 10px;
+  background: none;
+  font-size: 16px;
+  color: #ddd;
+  text-transform: inherit;
+  transition: border-color 0.2;
+}
+.contact-form-button {
+  background: none;
+  border: none;
+  color: #4d4d4f;
+  font-size: 16px;
+  cursor: pointer;
+  outline: none;
+}
+.contact-form-button:hover {
+  color: #fff;
+}
+
+.contact-form-edit {
+  width: 30%;
+  padding: 10px;
+  background: none;
+  border: none;
+  border-bottom: 1px solid #666;
+  color: #ddd;
+  font-size: 16px;
+  text-transform: capitalize;
+  outline: none;
+  transition: border-color 0.2s;
+}
 </style>
